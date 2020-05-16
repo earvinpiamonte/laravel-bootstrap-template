97d063e840ea8b952230d26d2a4e9ccdd7704f5f
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-05-16 14:52:23 +0800

    Update webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,35 @@
-const mix = require('laravel-mix');
-
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+|--------------------------------------------------------------------------
+| Mix Asset Management
+|--------------------------------------------------------------------------
+|
+| Mix provides a clean, fluent API for defining some Webpack build steps
+| for your Laravel application. By default, we are compiling the Sass
+| file for the application as well as bundling up all the JS files.
+|
+*/
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+const mix = require("laravel-mix");
+
+const ENV = process.env;
+
+mix.browserSync({
+    proxy: ENV.MIX_SENTRY_DSN_PUBLIC,
+    open: false,
+    files: [
+        "app/**/*.php",
+        "resources/views/**/*.php",
+        "public/js/**/*.js",
+        "public/css/**/*.css"
+    ],
+    watchOptions: {
+        usePolling: true
+    }
+})
+    .js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+    .disableNotifications();
+
+if (mix.inProduction()) {
+    mix.version();
+}
